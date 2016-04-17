93e5b5f0920fd0b487d192189be61d4f27cb8b39
James <james@JamesWMAC.local>
2016-04-17 16:48:39 +1000

    Add DEV option, print out messages
    Print out action being triggered, the reducers which will be persisted,
    and the store after the persited sync.

--- src/storage.js
+++ src/storage.js
@@ -1,11 +1,18 @@
 import { AsyncStorage } from 'react-native'
 
-const storage = keys => {
+const storage = keys => dev => {
   return store => next => action => {
     next(action)
     let state = store.getState()
+    if (dev) {
+		console.log ("\nAction has been triggered: ", action);
+		console.log ("\nReducers that will be synced to AsyncStorage: ", keys);
+		console.log ("\nStore after async ", state);
+    }
+
+    // Remove .toJS()
     for(let item in state) {
-      if(keys.indexOf(item) > -1) AsyncStorage.setItem(item, JSON.stringify(state[item].toJS()))
+      if(keys.indexOf(item) > -1) AsyncStorage.setItem(item, JSON.stringify(state[item]))
     }
   }
 }
